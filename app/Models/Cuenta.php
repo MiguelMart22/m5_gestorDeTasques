96a9d7c866b3e9911144344631eb9462bdc5354b
miguel.martinez <miguel.martinez35j@gmail.com>
2024-01-28 16:28:43 +0100

    models cuenta,gestor de tasques i tests cuenta,tasques

---
+++ app/Models/Cuenta.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\Date;
+use Exception;
+
+/**
+ * 
+ */
+class Cuenta{
+    private float $saldo;
+    public function __construct(){
+        $this-> saldo=0.0;
+    }
+
+    public function getSaldo():float {
+        return $this-> saldo;
+    }
+
+    public function ingresar(float $quantitat):void {
+        $quantitatString = strval($quantitat);
+        
+        if($quantitat > 0 && strpos($quantitatString, ".")+2 < strlen($quantitatString)){
+            $this->saldo += $quantitat;
+        }
+    }
+
+    public function transferir(Cuenta $cuenta,float $quantitat):void {
+        $this->saldo-=$quantitat;
+        $cuenta+=$quantitat;
+    }
+
+}
